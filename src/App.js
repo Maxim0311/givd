bbd8089b96591b08e194a3fb3ed90fca823ce826
maxim0311 <maxim0311@mail.ru>
2022-12-04 18:17:16 +0300

    lr7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useServerGoods } from './hooks/useServerGoods';
+import { useEffect, useState } from 'react';
+import PassangerItem from './components/PassangerItem';
 
 function App() {
+  const { fetchData, items, loading, error } = useServerGoods();
+
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(3);
+
+  useEffect(() => {
+    fetchData(page, size);
+  }, []);
+
+  const showMore = () => {
+    setSize(s => s + 3);
+    fetchData(page, size);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && 'Loading...'}
+      {items?.data?.map(item => (
+        <PassangerItem _id={item?._id} name={item?.name} trips={item?.trips} />
+      ))}
+      {items?.totalPages > 1 && (
+        <button onClick={showMore}>Показать больше...</button>
+      )}
     </div>
   );
 }
